3fa37193f4b709e713fa4fcdcc6e2e59e25587a2
andycat <andycat@localhost>
2009-05-13 09:48:38 +0000

    vocabularies
    svn path=/plumi.app/trunk/; revision=85969

---
+++ plumi/app/vocabs.py
@@ -0,0 +1,57 @@
+
+vocab_set = {}
+
+vocab_set['video_categories'] = (
+         ('poverty', u'Poverty / Development'),
+         ('indigenous', u'Indigenous'),
+         ('refugee', u'Refugee / Migration'),
+         ('health', u'Health'),
+         ('corporations', u'Corporations / Privatisation'),
+         ('globalisation', u'Globalisation'),
+         ('law', u'Law / Justice'),
+         ('work', u'Work'),
+         ('consumerism', u'Consumerism'),
+         ('war', u'War / Peace'),
+         ('human', u'Human Rights'),
+         ('disability', u'Disability Rights'),
+         ('gender', u'Gender / Sexuality'),
+         ('race', u'Race'),
+         ('religion', u'Religion'),
+         ('art', u'Art / Culture'),
+         ('internet', u'Internet'),
+         ('media', u'Media'),
+         ('activism', u'Activism'),
+         ('politics', u'Politics'),
+         ('education', u'Education'),
+         ('biodiversity', u'Biodiversity'),
+         ('climate', u'Climate Change'),
+         ('conservation', u'Forests / Conservation'),
+         ('nuclear', u'Nuclear'),
+         ('sustainablity', u'Sustainability'),
+         ('animal', u'Animal Rights'),
+         ('water', u'Water'),
+         ('biotech', u'Biotech'),
+         ('civillib',u'Civil Liberties'),
+        )
+vocab_set['video_genre'] = (
+         ('documentary', u'Documentary'),
+         ('experimental', u'Experimental'),
+         ('fiction', u'Fiction'),
+         ('animation', u'Animation'),
+         ('music', u'Music'),
+         ('newsreport', u'News Report'),
+        )
+vocab_set['submission_categories'] = (
+         ('festival', u'Festival'),
+         ('screening', u'Screening'),
+         ('dvd', u'DVD'),
+         ('production', u'Production'),
+         ('conference', u'Conference'),
+         ('workshop', u'Workshop'),
+         ('crew', u'Crew'),
+         ('competition', u'Competition'),
+         ('artprize', u'Art Prize'),
+         ('exhibition',u'Exhibition'),
+         ('other', u'Other'),
+        ) 
+
