4ef3b94ce29661b294e9bb834e79aeae6caa850e
andycat <andycat@localhost>
2009-05-13 10:55:11 +0000

    add in config.py file
    atcountrywidget sw-asia custom configuration
    
    lowercase for vaporisatoin egg install
    
    
    svn path=/plumi.app/trunk/; revision=85979

--- plumi/app/__init__.py
+++ plumi/app/__init__.py
@@ -8,6 +8,7 @@ except ImportError:
 import logging
 
 from plumi.app.vocabs  import vocab_set as vocabs
+from plumi.app import config
 
 def initialize(context):  
     """Initializer called when used as a Zope 2 product."""
@@ -22,11 +23,13 @@ def app_installation_tasks(self):
     logger=logging.getLogger('plumi.app')
 
     logger.info('starting app_installation_tasks. self is %s' %self)
-
+    #imports from old style plone 'Products' namespace
     from Products.CMFCore.utils import getToolByName
+    from Products.ATVocabularyManager.config import TOOL_NAME as ATVOCABULARYTOOL
+    from Products.ATCountryWidget.CountryTool import CountryUtils, Country
+
     #ATVocabManager setup
     logger.info('Starting ATVocabManager configuration')
-    from Products.ATVocabularyManager.config import TOOL_NAME as ATVOCABULARYTOOL
     portal=getToolByName(self,'portal_url').getPortalObject()
     atvm = getToolByName(portal, ATVOCABULARYTOOL)
      
@@ -48,4 +51,36 @@ def app_installation_tasks(self):
                 logger.debug("adding vocabulary item %s %s" % (ikey,value))
                 vocab[ikey].setTitle(value)
 
-    logger.info('starting ATCountryWidget configuration')
+    #ATCountryWidget setup
+    #reset the country tool
+    countrytool = getToolByName(self, CountryUtils.id)
+    #common code
+    countrytool.manage_countries_reset()
+        
+    if config.SE_ASIA_COUNTRIES:
+        logger.info('starting custom se-asia countries ATCountryWidget configuration')
+        #Add countries missing from the standard installs
+        # 
+        countrytool.manage_countries_addCountry('WP','West Papua')
+        #XXX this is a state of America, not a country - but thats just politics!!
+        countrytool.manage_countries_addCountry('HA','Hawaii')
+        countrytool.manage_countries_addCountry('BU','Bougainville')
+       
+        #update three more pre-exisiting countries 
+        #change Myanmar to Burma
+        #Lao Peoples blah blah to just Laos
+        #Viet Nam to Vietnam
+        countries = countrytool._country_list
+        countries[countries.index(Country('MM'))].name="Burma"
+        countries[countries.index(Country('LA'))].name="Laos"
+        countries[countries.index(Country('VN'))].name="Vietnam"
+        countries[countries.index(Country('NZ'))].name="NZ-Aotearoa"
+      
+        #add our areas
+        countrytool.manage_countries_addArea('South East Asia')
+        countrytool.manage_countries_addCountryToArea('South East Asia', ['SG','TH','VN','ID','PH','LA','MY','KH','BN','MM'])
+        countrytool.manage_countries_sortArea('South East Asia')
+     
+        countrytool.manage_countries_addArea('Oceania')
+        countrytool.manage_countries_addCountryToArea('Oceania', ['AU','NZ','PG','TL','WP','FJ','SB','HA','NC','VU','WS','BU'    ,'NR','TO','TV','GU','KI','FM','PF','MH','MP','PW','PN','TK','AQ'])
+        countrytool.manage_countries_sortArea('Oceania')
---
+++ plumi/app/config.py
@@ -0,0 +1,1 @@
+SE_ASIA_COUNTRIES=True
