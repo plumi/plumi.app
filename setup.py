b5e78cdefeaae1f09141f3d7d5a86a5ff4a8f6cd
andycat <andycat@localhost>
2009-07-24 02:12:00 +0000

    bump version
    svn path=/plumi.app/trunk/; revision=93067

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1'
+version = '0.2'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
