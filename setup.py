3a657f3a0c0421b5ac70b33bde9baca6c83623b5
clopy <clopy@localhost>
2010-04-11 19:00:24 +0000

    Change to 0.5 version
    svn path=/plumi.app/trunk/; revision=115070

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.4'
+version = '0.5'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
