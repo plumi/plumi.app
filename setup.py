7b5e8de02570c7d6e2141e62751dd7dd2fd42822
Dimitris Moraitis <dimo@unweb.me>
2010-04-27 15:35:57 +0000

    Prepare plumi.app 0.6.
    svn path=/plumi.app/trunk/; revision=116271

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.5'
+version = '0.6'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
