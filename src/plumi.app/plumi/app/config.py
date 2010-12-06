af2020f972dc06d4a1af5302a1d2ac11ff1e050e
clopy <clopy@localhost>
2010-12-06 14:29:51 +0000

    update vocabs, move taxonomy generation in a separate product
    svn path=/plumi.app/trunk/; revision=228323

--- src/plumi.app/plumi/app/config.py
+++
@@ -1,9 +0,0 @@
-#setup the countries tool only for SE Asia
-SE_ASIA_COUNTRIES=True
-
-#Names of the folders inside the taxonomy hierarchy
-TOPLEVEL_TAXONOMY_FOLDER='taxonomy'
-GENRE_FOLDER='genre'
-CATEGORIES_FOLDER='topic'
-COUNTRIES_FOLDER='countries'
-SUBMISSIONS_FOLDER='callouts'
--- src/plumi.app/plumi/app/vocabs.py
+++ src/plumi.app/plumi/app/vocabs.py
@@ -6,6 +6,7 @@ vocab_set = {}
 taxonomy_sub_folder={'topic':'video_categories','genre':'video_genre','callouts':'submission_categories','countries':'video_countries'}
 
 vocab_set['video_countries'] = (
+         ('XX', _(u'----------------')),
          ('AF', _(u'Afghanistan')),
          ('AL', _(u'Albania')),
          ('DZ', _(u'Algeria ')),
@@ -249,56 +250,19 @@ vocab_set['video_countries'] = (
         )
 
 vocab_set['video_categories'] = (
-         ('poverty', _(u'Poverty / Development')),
-         ('indigenous', _(u'Indigenous')),
-         ('refugee', _(u'Refugee / Migration')),
-         ('health', _(u'Health')),
-         ('corporations', _(u'Corporations / Privatisation')),
-         ('globalisation', _(u'Globalisation')),
-         ('law', _(u'Law / Justice')),
-         ('work', _(u'Work')),
-         ('consumerism', _(u'Consumerism')),
-         ('war', _(u'War / Peace')),
-         ('human', _(u'Human Rights')),
-         ('disability', _(u'Disability Rights')),
-         ('gender', _(u'Gender / Sexuality')),
-         ('race', _(u'Race')),
-         ('religion', _(u'Religion')),
          ('art', _(u'Art / Culture')),
          ('internet', _(u'Internet')),
-         ('media', _(u'Media')),
-         ('activism', _(u'Activism')),
-         ('politics', _(u'Politics')),
-         ('education', _(u'Education')),
-         ('biodiversity', _(u'Biodiversity')),
-         ('climate', _(u'Climate Change')),
-         ('conservation', _(u'Forests / Conservation')),
-         ('nuclear', _(u'Nuclear')),
-         ('sustainablity', _(u'Sustainability')),
-         ('animal', _(u'Animal Rights')),
-         ('water', _(u'Water')),
-         ('biotech', _(u'Biotech')),
-         ('civillib',_(u'Civil Liberties')),
         )
 vocab_set['video_genre'] = (
+         ('none', _(u'----------------')),
          ('documentary', _(u'Documentary')),
-         ('experimental', _(u'Experimental')),
          ('fiction', _(u'Fiction')),
          ('animation', _(u'Animation')),
-         ('music', _(u'Music')),
-         ('newsreport', _(u'News Report')),
         )
 vocab_set['submission_categories'] = (
-         ('festival', _(u'Festival')),
-         ('screening', _(u'Screening')),
+         ('XX', _(u'----------------')),
          ('dvd', _(u'DVD')),
          ('production', _(u'Production')),
-         ('conference', _(u'Conference')),
-         ('workshop', _(u'Workshop')),
-         ('crew', _(u'Crew')),
-         ('competition', _(u'Competition')),
-         ('artprize', _(u'Art Prize')),
-         ('exhibition',_(u'Exhibition')),
          ('other', _(u'Other')),
         ) 
 
