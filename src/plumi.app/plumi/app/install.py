90116ce5df68411bdd66ec3def02416409e9194d
clopy <clopy@localhost>
2011-02-20 08:53:47 +0000

    only crate torrents for published and featured videos on upgrade
    svn path=/plumi.app/trunk/; revision=232379

--- src/plumi.app/plumi/app/install.py
+++ src/plumi.app/plumi/app/install.py
@@ -477,49 +477,55 @@ def plumi4to41(context, logger=None):
     catalog = getToolByName(context, 'portal_catalog')
     # Create torrents for videos
     videos = catalog(portal_type='PlumiVideo')
+    workflowTool = getToolByName(context, "portal_workflow")
     for video in videos:
         obj = video.getObject()
         if not obj.UID():
             return
         try:
-            registry = getUtility(IRegistry)
-            types = registry['collective.seeder.interfaces.ISeederSettings.portal_types']
-            torrent_dir = registry['collective.seeder.interfaces.ISeederSettings.torrent_dir']
-            announce_urls = registry['collective.seeder.interfaces.ISeederSettings.announce_urls']
-            safe_torrent_dir = registry['collective.seeder.interfaces.ISeederSettings.safe_torrent_dir']
-            #Check if the torrent directory exists, otherwise create it
-            if not os.path.exists(torrent_dir):
-                os.makedirs(torrent_dir)
-            #Check if the torrent safe directory exists, otherwise create it
-            if not os.path.exists(safe_torrent_dir):
-                os.makedirs(safe_torrent_dir)
-            newTypes = [t.split(':')[0] for t in types]
-            if unicode(obj.portal_type) not in newTypes:
-                return
-            fieldNames = [str(t.split(':')[1]) for t in types if ('%s:' % unicode(obj.portal_type)) in t]
-            fts= Zope2.DB._storage.fshelper
-            if not fieldNames:
-                fields = [obj.getPrimaryField()]
-            else:
-                fields = [obj.getField(f) for f in fieldNames]
-            for field in fields:
-                oid = field.getUnwrapped(obj).getBlob()._p_oid
-                if not oid:
+            status = workflowTool.getStatusOf("plumi_workflow", obj)
+            state = status["review_state"]
+            if (state == "published") or (state == "featured"):
+                registry = getUtility(IRegistry)
+                types = registry['collective.seeder.interfaces.ISeederSettings.portal_types']
+                torrent_dir = registry['collective.seeder.interfaces.ISeederSettings.torrent_dir']
+                announce_urls = registry['collective.seeder.interfaces.ISeederSettings.announce_urls']
+                safe_torrent_dir = registry['collective.seeder.interfaces.ISeederSettings.safe_torrent_dir']
+                #Check if the torrent directory exists, otherwise create it
+                if not os.path.exists(torrent_dir):
+                    os.makedirs(torrent_dir)
+                #Check if the torrent safe directory exists, otherwise create it
+                if not os.path.exists(safe_torrent_dir):
+                    os.makedirs(safe_torrent_dir)
+                newTypes = [t.split(':')[0] for t in types]
+                if unicode(obj.portal_type) not in newTypes:
                     return
-                #Symlink the blob object to the original filename
-                blobDir = fts.getPathForOID(oid)
-                blobName = os.listdir(blobDir)
-                blobPath = os.path.join(blobDir,blobName[0])
-                fileName = obj.UID() + '_' + field.getFilename(obj)
-                linkPath = os.path.join(torrent_dir, fileName)
-                os.symlink(blobPath, linkPath)
+                fieldNames = [str(t.split(':')[1]) for t in types if ('%s:' % unicode(obj.portal_type)) in t]
+                fts= Zope2.DB._storage.fshelper
+                if not fieldNames:
+                    fields = [obj.getPrimaryField()]
+                else:
+                    fields = [obj.getField(f) for f in fieldNames]
+                for field in fields:
+                    oid = field.getUnwrapped(obj).getBlob()._p_oid
+                    if not oid:
+                        return
+                    #Symlink the blob object to the original filename
+                    blobDir = fts.getPathForOID(oid)
+                    blobName = os.listdir(blobDir)
+                    blobPath = os.path.join(blobDir,blobName[0])
+                    fileName = obj.UID() + '_' + field.getFilename(obj)
+                    linkPath = os.path.join(torrent_dir, fileName)
+                    os.symlink(blobPath, linkPath)
 
-                torrentName = fileName + '.torrent'
-                torrentPath = os.path.join(torrent_dir, torrentName)
-                make_meta_file(linkPath, str(",".join(str(v) for v in announce_urls)), 262144)
+                    torrentName = fileName + '.torrent'
+                    torrentPath = os.path.join(torrent_dir, torrentName)
+                    make_meta_file(linkPath, str(",".join(str(v) for v in announce_urls)), 262144)
 
-                torrentSafePath = safe_torrent_dir + '/' + fileName + '.torrent'
-                shutil.copyfile(torrentPath, torrentSafePath)      
+                    torrentSafePath = safe_torrent_dir + '/' + fileName + '.torrent'
+                    shutil.copyfile(torrentPath, torrentSafePath)      
+            else:
+                pass
         except:
             pass     
 
