05a9a5d23087d0f9382455902f8b70461cdcea53
Dimitris Moraitis <dimo@unweb.me>
2011-03-09 18:40:02 +0000

    prepare 4.1.1 release
    svn path=/plumi.app/trunk/; revision=235769

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '4.1'
+version = '4.1.1'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
@@ -22,7 +22,7 @@ long_description = (
 
 setup(name='plumi.app',
       version=version,
-      description="Plumi application setup",
+      description="Plumi video sharing",
       long_description=long_description,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
