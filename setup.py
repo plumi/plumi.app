5f9508fe3c432eb79a493e17238b60df92cf3b01
Dimitris Moraitis <dimo@unweb.me>
2011-05-11 10:14:21 +0000

    automatically create plumi site on first buildout run
    svn path=/plumi.app/trunk/; revision=239023

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '4.2rc1'
+version = '4.2rc2'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
