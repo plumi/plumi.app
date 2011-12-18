f59ae1cf629757daf59ad12c3a4e0337e6f32703
Dimitris Moraitis <dimo@unweb.me>
2011-12-18 20:08:38 +0200

    pin dependencies, update changelog

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '4.3'
+version = '4.3rc1'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
