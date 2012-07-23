e06772caf41c3f6caaa4b9c91e564510364d3c06
Dimitris Moraitis <dimo@unweb.me>
2012-07-23 19:12:42 +0300

    update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '4.4b1'
+version = '4.4rc1'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
