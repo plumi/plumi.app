211022cd8cd303268a6f5b7de1837c13ec5e8bb7
Dimitris Moraitis <dimo@unweb.me>
2012-07-26 15:21:20 +0300

    pin 4.4 packages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '4.4rc1'
+version = '4.4rc2'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
