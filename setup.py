c2f6994c30952f207936064f5ee0e924e2ec9e4b
Dimitris Moraitis <dimo@unweb.me>
2012-11-18 15:16:02 +0200

    prepare plumi.app 4.5b1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '4.5dev'
+version = '4.5b1'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
