c580ce3eac2e9924b96d5bbe997326898487d8d2
Markos Gogoulos <mgogoulos@mist.io>
2016-11-17 13:11:18 +0000

    em specific

--- src/plumi.app/plumi/app/member_area.py
+++ src/plumi.app/plumi/app/member_area.py
@@ -44,11 +44,14 @@ def notifyMemberAreaCreated(container,context):
 
     logger.info('found member, is %s ' % member_id) 
     # here we do the copying
-    copy_ids = default_member_content.objectIds()
+#    copy_ids = default_member_content.objectIds()
+    copy_ids = ['callouts', 'videos', 'news', 'events']
     #exclude
     id_filter = ['.personal']
     copy_ids = [id for id in copy_ids if id not in id_filter]
-    objects = default_member_content.manage_copyObjects(copy_ids)
+#    objects = default_member_content.manage_copyObjects(copy_ids)
+    objects = 'x%DA%D3%60b%60%60%C8%04b%86h%16%20%A1%01%22J%40%DCbV%20%11%90%93%9F%97Z%2C%06d%A5%A4%A6%25%96%E6%94%C4%E7%A6%E6%26%A5%16%C5%27%E7%E7%95%A4%E6%95%14s%00%A5%92%13sr%F2KK%8A%C1z%83%88%D7%CB%06%94%2A%CBLI%CD%27Y%27Hy%5Ej9Y6%A6%96%81X%00%20%7B%3F%88'
+
     context.manage_pasteObjects(objects)
     logger.info('copied objects from default member content')
  
